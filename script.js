057ee4c92c094f3aa41a1f281a1766a0432e7ef8
Neveen1803 <128345749+Neveen1803@users.noreply.github.com>
2024-06-05 21:40:59 +0530

    Add files via upload
    Added Portfolio files

---
+++ script.js
@@ -0,0 +1,207 @@
+
+window.onload=function(){
+    document.getElementById("JAVA1").style.display="none";
+    document.getElementById("JAVA2").style.display="none";
+    document.getElementById("JAVA3").style.display="none";
+    document.getElementById("Todo").style.display="none";
+    document.getElementById("TicTacToe").style.display="none";
+    document.getElementById("CoreJava1").style.display="none";
+    document.getElementById("AdvanceJava1").style.display="block";
+    document.getElementById("HTML1").style.display="none"
+    document.getElementById("HTML2").style.display="none"
+    document.getElementById("HTML3").style.display="none"
+  
+    document.getElementById("MyBest").style.backgroundColor="#68482a";
+    document.getElementById("MyBest").style.color="white";
+
+}
+
+function Scratch(){
+    window.location.href="https://scratch.mit.edu/projects/723867888/";
+}
+
+function JS() {
+    document.getElementById("CoreJava1").style.display="none";
+
+    document.getElementById("JS1").style.display="block";
+    document.getElementById("JAVA1").style.display="none";
+    document.getElementById("JAVA2").style.display="none";
+    document.getElementById("JAVA3").style.display="none";
+    document.getElementById("ScratchProject").style.display="none";
+    document.getElementById("Todo").style.display="block";
+    document.getElementById("TicTacToe").style.display="block";
+    document.getElementById("AdvanceJava1").style.display="none";
+    document.getElementById("HTML1").style.display="none"
+    document.getElementById("HTML2").style.display="none"
+    document.getElementById("HTML3").style.display="none"
+
+document.getElementById("MyBest").style.backgroundColor="#f5f5f5";
+document.getElementById("MyBest").style.color="black";
+document.getElementById("HTML").style.backgroundColor="#f5f5f5";
+document.getElementById("HTML").style.color="black";
+document.getElementById("JS").style.backgroundColor="#68482a";
+document.getElementById("JS").style.color="white";
+
+document.getElementById("AdvancedHava").style.backgroundColor="#f5f5f5";
+document.getElementById("AdvancedHava").style.color="black";
+document.getElementById("Scratch").style.backgroundColor="#f5f5f5";
+document.getElementById("Scratch").style.color="black";
+document.getElementById("JAVA").style.backgroundColor="#f5f5f5";
+    document.getElementById("JAVA").style.color="black";
+
+}
+function JAVA() {
+    document.getElementById("TicTacToe").style.display="none";
+    document.getElementById("CoreJava1").style.display="block";
+    document.getElementById("AdvanceJava1").style.display="none";
+
+    document.getElementById("JS1").style.display="none";
+    document.getElementById("JAVA1").style.display="block";
+    document.getElementById("JAVA2").style.display="none";
+    document.getElementById("JAVA3").style.display="block";
+    document.getElementById("ScratchProject").style.display="none";
+    document.getElementById("Todo").style.display="none";
+    document.getElementById("HTML1").style.display="none"
+    document.getElementById("HTML2").style.display="none"
+    document.getElementById("HTML3").style.display="none"
+
+    document.getElementById("MyBest").style.backgroundColor="#f5f5f5";
+document.getElementById("MyBest").style.color="black";
+document.getElementById("HTML").style.backgroundColor="#f5f5f5";
+document.getElementById("HTML").style.color="black";
+document.getElementById("JS").style.backgroundColor="#f5f5f5";
+document.getElementById("JS").style.color="black";
+document.getElementById("JAVA").style.backgroundColor="#68482a";
+document.getElementById("JAVA").style.color="white";
+
+document.getElementById("AdvancedHava").style.backgroundColor="#f5f5f5";
+document.getElementById("AdvancedHava").style.color="black";
+document.getElementById("Scratch").style.backgroundColor="#f5f5f5";
+document.getElementById("Scratch").style.color="black";
+}
+function SCRATCH() {
+    document.getElementById("TicTacToe").style.display="none";
+    document.getElementById("CoreJava1").style.display="none";
+    document.getElementById("AdvanceJava1").style.display="none";
+
+    document.getElementById("JS1").style.display="none";
+    document.getElementById("JAVA1").style.display="none";
+    document.getElementById("JAVA2").style.display="none";
+    document.getElementById("JAVA3").style.display="none";
+    document.getElementById("ScratchProject").style.display="block";
+    document.getElementById("Todo").style.display="none";
+    document.getElementById("HTML1").style.display="none"
+    document.getElementById("HTML2").style.display="none"
+    document.getElementById("HTML3").style.display="none"
+
+    document.getElementById("MyBest").style.backgroundColor="#f5f5f5";
+    document.getElementById("MyBest").style.color="black";
+    document.getElementById("HTML").style.backgroundColor="#f5f5f5";
+    document.getElementById("HTML").style.color="black";
+    document.getElementById("JS").style.backgroundColor="#f5f5f5";
+    document.getElementById("JS").style.color="black";
+    document.getElementById("JAVA").style.backgroundColor="#f5f5f5";
+    document.getElementById("JAVA").style.color="black";
+   
+    document.getElementById("AdvancedHava").style.backgroundColor="#f5f5f5";
+    document.getElementById("AdvancedHava").style.color="black";
+    document.getElementById("Scratch").style.backgroundColor="#68482a";
+    document.getElementById("Scratch").style.color="white";
+}
+function Mybest(){
+    document.getElementById("CoreJava1").style.display="none";
+    document.getElementById("AdvanceJava1").style.display="block";
+
+    document.getElementById("TicTacToe").style.display="none";
+
+    document.getElementById("JS1").style.display="block";
+    document.getElementById("JAVA1").style.display="none";
+    document.getElementById("JAVA2").style.display="none";
+    document.getElementById("JAVA3").style.display="none";
+    document.getElementById("ScratchProject").style.display="block";
+    document.getElementById("Todo").style.display="none";
+    document.getElementById("HTML1").style.display="none";
+    document.getElementById("HTML2").style.display="none";
+    document.getElementById("HTML3").style.display="none";
+
+    document.getElementById("MyBest").style.backgroundColor="#68482a";
+    document.getElementById("MyBest").style.color="white";
+    document.getElementById("HTML").style.backgroundColor="#f5f5f5";
+    document.getElementById("HTML").style.color="black";
+    document.getElementById("JS").style.backgroundColor="#f5f5f5";
+    document.getElementById("JS").style.color="black";
+   
+    document.getElementById("AdvancedHava").style.backgroundColor="#f5f5f5";
+    document.getElementById("AdvancedHava").style.color="black";
+    document.getElementById("Scratch").style.backgroundColor="#f5f5f5";
+    document.getElementById("Scratch").style.color="black";
+    document.getElementById("JAVA").style.backgroundColor="#f5f5f5";
+    document.getElementById("JAVA").style.color="black";
+
+
+}
+
+function Servlet() {
+    document.getElementById("JS1").style.display="none";
+    document.getElementById("JAVA1").style.display="none";
+    document.getElementById("JAVA2").style.display="block";
+    document.getElementById("JAVA3").style.display="none";
+    document.getElementById("Todo").style.display="none";
+    document.getElementById("TicTacToe").style.display="none";
+    document.getElementById("CoreJava1").style.display="none";
+    document.getElementById("ScratchProject").style.display="none";
+    document.getElementById("AdvanceJava1").style.display="block";
+document.getElementById("HTML1").style.display="none"
+document.getElementById("HTML2").style.display="none"
+document.getElementById("HTML3").style.display="none"
+
+
+document.getElementById("MyBest").style.backgroundColor="#f5f5f5";
+document.getElementById("MyBest").style.color="black";
+document.getElementById("HTML").style.backgroundColor="#f5f5f5";
+document.getElementById("HTML").style.color="black";
+document.getElementById("JS").style.backgroundColor="#f5f5f5";
+document.getElementById("JS").style.color="black";
+
+document.getElementById("AdvancedHava").style.backgroundColor="#68482a";
+document.getElementById("AdvancedHava").style.color="white";
+document.getElementById("Scratch").style.backgroundColor="#f5f5f5";
+document.getElementById("Scratch").style.color="black";
+document.getElementById("JAVA").style.backgroundColor="#f5f5f5";
+    document.getElementById("JAVA").style.color="black";
+
+
+}
+function HTML() {
+    document.getElementById("JS1").style.display="none";
+    document.getElementById("JAVA1").style.display="none";
+    document.getElementById("JAVA2").style.display="none";
+    document.getElementById("JAVA3").style.display="none";
+    document.getElementById("Todo").style.display="none";
+    document.getElementById("TicTacToe").style.display="none";
+    document.getElementById("CoreJava1").style.display="none";
+    document.getElementById("ScratchProject").style.display="none";
+    document.getElementById("AdvanceJava1").style.display="none";
+document.getElementById("HTML1").style.display="block"
+document.getElementById("HTML2").style.display="block"
+document.getElementById("HTML3").style.display="block"
+
+
+
+
+document.getElementById("MyBest").style.backgroundColor="#f5f5f5";
+document.getElementById("MyBest").style.color="black";
+document.getElementById("HTML").style.backgroundColor="#68482a";
+document.getElementById("HTML").style.color="white";
+document.getElementById("JS").style.backgroundColor="#f5f5f5";
+document.getElementById("JS").style.color="black";
+
+document.getElementById("AdvancedHava").style.backgroundColor="#f5f5f5";
+document.getElementById("AdvancedHava").style.color="black";
+document.getElementById("Scratch").style.backgroundColor="#f5f5f5";
+document.getElementById("Scratch").style.color="black";
+document.getElementById("JAVA").style.backgroundColor="#f5f5f5";
+    document.getElementById("JAVA").style.color="black";
+
+
+}
